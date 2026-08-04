0e925fafe3d270fe1c64f00da5581c0fc538c2b0
dan5 <kepa.kachi310@gmail.com>
2026-08-04 20:36:36 +0900

    kansei??

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
-}
+
+  server: {
+    open: true,
+  },
+});
