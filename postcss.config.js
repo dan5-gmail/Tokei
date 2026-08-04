6e675ac3f0b591c639bb447299c43aade10141c6
dan5-gmail <kepa.kachi310@gmail.com>
2026-08-04 21:04:34 +0900

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-
-  server: {
-    open: true,
-  },
-});
+}
